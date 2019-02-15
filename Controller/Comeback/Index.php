dfebb5996d96b5db2fca0a6a7873969ee25ec297
Mikel <mikel@zhenit.com>
2019-02-15 12:38:54 +0100

    [MP2-8] Empty minicart

--- Controller/Comeback/Index.php
+++ Controller/Comeback/Index.php
@@ -5,15 +5,87 @@
 
 namespace Sequra\Core\Controller\Comeback;
 
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Session\SessionManagerInterface;
+use Magento\Framework\Stdlib\CookieManagerInterface;
+use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
+
 /**
  * Comeback controller
  */
 class Index extends \Magento\Checkout\Controller\Onepage
 {
     /**
+     * @var \Magento\Framework\Session\SessionManagerInterface
+     */
+    protected $cookieManager;
+
+    /**
+     * @var \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory
+     */
+    protected $cookieMetadataFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context              $context
+     * @param \Magento\Customer\Model\Session                    $customerSession
+     * @param CustomerRepositoryInterface                        $customerRepository
+     * @param AccountManagementInterface                         $accountManagement
+     * @param \Magento\Framework\Registry                        $coreRegistry
+     * @param \Magento\Framework\Translate\InlineInterface       $translateInline
+     * @param \Magento\Framework\Data\Form\FormKey\Validator     $formKeyValidator
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\View\LayoutFactory              $layoutFactory
+     * @param \Magento\Quote\Api\CartRepositoryInterface         $quoteRepository
+     * @param \Magento\Framework\View\Result\PageFactory         $resultPageFactory
+     * @param \Magento\Framework\View\Result\LayoutFactory       $resultLayoutFactory
+     * @param \Magento\Framework\Controller\Result\RawFactory    $resultRawFactory
+     * @param \Magento\Framework\Controller\Result\JsonFactory   $resultJsonFactory
+     *
+     * @codeCoverageIgnore
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        CustomerRepositoryInterface $customerRepository,
+        AccountManagementInterface $accountManagement,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Framework\Translate\InlineInterface $translateInline,
+        \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        CookieManagerInterface $cookieManager,
+        CookieMetadataFactory $cookieMetadataFactory
+    ) {
+        parent::__construct(
+            $context,
+            $customerSession,
+            $customerRepository,
+            $accountManagement,
+            $coreRegistry,
+            $translateInline,
+            $formKeyValidator,
+            $scopeConfig,
+            $layoutFactory,
+            $quoteRepository,
+            $resultPageFactory,
+            $resultLayoutFactory,
+            $resultRawFactory,
+            $resultJsonFactory
+        );
+        $this->cookieMetadataFactory = $cookieMetadataFactory;
+        $this->cookieManager = $cookieManager;
+    }
+    /**
      * Rebuild session and redirect to default success controller
      *
-     * @return void
+     * @return                                 void
      * @SuppressWarnings(PHPMD.ExitExpression)
      */
     public function execute()
@@ -33,6 +105,17 @@ class Index extends \Magento\Checkout\Controller\Onepage
             ->setLastSuccessQuoteId($quote->getId())
             ->setLastOrderId($order->getId())
             ->setLastRealOrderId($quote->getReservedOrderId());
+        // Reset mincart.
+        $metadata = $this->cookieMetadataFactory
+            ->createPublicCookieMetadata()
+            ->setPath('/');
+        $sectiondata = json_decode($this->cookieManager->getCookie('section_data_ids'));
+        $sectiondata->cart += 1000;
+        $this->cookieManager->setPublicCookie(
+            'section_data_ids',
+            json_encode($sectiondata),
+            $metadata
+        );
         return $this->resultRedirectFactory->create()->setPath('checkout/onepage/success');
     }
 }
