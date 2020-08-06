99f3f652141bd5a623470da4f4eb1a1a624bdd98
Mikel <mikel@zhenit.com>
2020-08-06 10:25:01 +0200

    [add] scafold in modifications needed

---
+++ Observer/SendMailOnOrderSuccess.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Sequra\Core\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class SendMailOnOrderSuccess implements ObserverInterface
+{
+    /**
+     * @var \Magento\Sales\Model\OrderFactory
+     */
+    protected $orderModel;
+
+    /**
+     * @var \Magento\Sales\Model\Order\Email\Sender\OrderSender
+     */
+    protected $orderSender;
+
+    /**
+     * @var \Magento\Checkout\Model\Session $checkoutSession
+     */
+    protected $checkoutSession;
+
+    /**
+     * @param \Magento\Sales\Model\OrderFactory $orderModel
+     * @param \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     *
+     * @codeCoverageIgnore
+     */
+    public function __construct(
+        \Magento\Sales\Model\OrderFactory $orderModel,
+        \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender,
+        \Magento\Checkout\Model\Session $checkoutSession
+    )
+    {
+        $this->orderModel = $orderModel;
+        $this->orderSender = $orderSender;
+        $this->checkoutSession = $checkoutSession;
+    }
+
+    /**
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        // $orderIds = $observer->getEvent()->getOrderIds();
+        // if (count($orderIds)) {
+        //     $this->checkoutSession->setForceOrderMailSentOnSuccess(true);
+        //     $order = $this->orderModel->create()->load($orderIds[0]);
+        //     $this->orderSender->send($order, true);
+        // }
+    }
+}
---
+++ Plugin/Sales/Order/Email/Container/OrderIdentity.php
@@ -0,0 +1,45 @@
+<?php
+namespace Sequra\Core\Plugin\Sales\Order\Email\Container;
+
+class OrderIdentity
+{
+    /**
+     * @var \Magento\Checkout\Model\Session $checkoutSession
+     */
+    protected $checkoutSession;
+
+    /**
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     *
+     * @codeCoverageIgnore
+     */
+    public function __construct(
+        \Magento\Checkout\Model\Session $checkoutSession
+    )
+    {
+        $this->checkoutSession = $checkoutSession;
+    }
+
+    /**
+     * @param \Magento\Sales\Model\Order\Email\Container\OrderIdentity $subject
+     * @param callable $proceed
+     * @return bool
+     */
+    public function aroundIsEnabled(\Magento\Sales\Model\Order\Email\Container\OrderIdentity $subject, callable $proceed)
+    {
+        $returnValue = $proceed();
+
+        $forceOrderMailSentOnSuccess = $this->checkoutSession->getForceOrderMailSentOnSuccess();
+        if(isset($forceOrderMailSentOnSuccess) && $forceOrderMailSentOnSuccess)
+        {
+            if($returnValue)
+                $returnValue = false;
+            else
+                $returnValue = true;
+
+            $this->checkoutSession->unsForceOrderMailSentOnSuccess();
+        }
+
+        return $returnValue;
+    }
+}
