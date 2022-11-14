b7abbd89108abde34d2309e4af3eb6bebb14bc52
Mikel <mikel@zhenit.com>
2022-11-14 13:06:32 +0100

    Typo

--- Model/Ui/ConfigProvider.php
+++ Model/Ui/ConfigProvider.php
@@ -58,7 +58,7 @@ class ConfigProvider implements ConfigProviderInterface
                     'scriptUri' => $this->config->getScriptUri(),
                     'decimalSeparator' => $this->getDecimalSeparator(),
                     'thousandSeparator' => $this->getThousandsSeparator(),
-                    'locale': str_replace('_','-',$this->localeResolver->getLocale()),
+                    'locale' => str_replace('_','-',$this->localeResolver->getLocale()),
                 ]
             ]
         ];
