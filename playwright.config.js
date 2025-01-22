1a50c0aabab7a441497634c16aa5998579c6a2c2
Michel Escalante Álvarez <michel.escalante@sequra.com>
2025-01-22 11:30:04 +0100

    Setup playwright
    Co-authored-by: mikel.martin@gmail.com <mikel.martin@gmail.com>

---
+++ playwright.config.js
@@ -0,0 +1,45 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests-e2e/specs',
+  timeout: 5 * 60 * 1000, // 5 minutes
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: 0,
+  /* Opt out of parallel tests on CI. */
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI ? 'dot' : 'list',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: process.env.PUBLIC_URL,
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'example',
+      use: { ...devices['Desktop Chrome'] },
+      testMatch: 'example.spec.js',
+    },
+  ],
+});
+
