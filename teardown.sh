6d60000294d45ca3f84758654fe0e1589ee609ac
Michel Escalante Álvarez <michel.escalante@sequra.com>
2025-03-06 16:48:43 +0100

    Refactor setup and teardown scripts to remove search engine profile handling and simplify Docker commands

--- teardown.sh
+++ teardown.sh
@@ -3,10 +3,10 @@ set -o allexport
 source .env
 set +o allexport
 
-search_engine_profile="elasticsearch"
-if [ "$M2_SEARCH_ENGINE_HOST" == 'opensearch' ]; then
-    search_engine_profile="opensearch"
-fi
+# search_engine_profile="elasticsearch"
+# if [ "$M2_SEARCH_ENGINE_HOST" == 'opensearch' ]; then
+#     search_engine_profile="opensearch"
+# fi
 
-docker compose --profile "$search_engine_profile" down --volumes --remove-orphans
+docker compose down --volumes --remove-orphans
 docker rm -f $NGROK_CONTAINER_NAME || true
