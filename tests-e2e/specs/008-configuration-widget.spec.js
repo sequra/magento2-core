6790aaac049a86279e31fb1563abc5e6eaa241d4
Michel Escalante Álvarez <michel.escalante@sequra.com>
2025-06-10 11:47:48 +0200

    Test widget visibility on product page

--- tests-e2e/specs/008-configuration-widget.spec.js
+++ tests-e2e/specs/008-configuration-widget.spec.js
@@ -59,8 +59,8 @@ test.describe('Widget settings', () => {
       { ...onlyProductSettings, product: { ...onlyProductSettings.product, altPriceTriggerSel: invalidSelector } },
       { ...onlyProductSettings, product: { ...onlyProductSettings.product, locationSel: emptyStr } },
       { ...onlyProductSettings, product: { ...onlyProductSettings.product, locationSel: invalidSelector } },
-      { ...onlyProductSettings, product: { ...onlyProductSettings.product, customLocations: [{ ...newSettings.product.customLocations[0], locationSel: invalidSelector}] } },
-      { ...onlyProductSettings, product: { ...onlyProductSettings.product, customLocations: [{ ...newSettings.product.customLocations[0], widgetConfig: invalidJSON}] } },
+      { ...onlyProductSettings, product: { ...onlyProductSettings.product, customLocations: [{ ...newSettings.product.customLocations[0], locationSel: invalidSelector }] } },
+      { ...onlyProductSettings, product: { ...onlyProductSettings.product, customLocations: [{ ...newSettings.product.customLocations[0], widgetConfig: invalidJSON }] } },
       { ...onlyCartSettings, cart: { ...onlyCartSettings.cart, priceSel: emptyStr } },
       { ...onlyCartSettings, cart: { ...onlyCartSettings.cart, priceSel: invalidSelector } },
       { ...onlyCartSettings, cart: { ...onlyCartSettings.cart, locationSel: emptyStr } },
@@ -94,81 +94,76 @@ test.describe('Widget settings', () => {
     await widgetSettingsPage.expectConfigurationMatches(newSettings);
   });
 
-  // test('Show widget', async ({ productPage, widgetSettingsPage, request }) => {
-  //   const defaultSettings = {
-  //     ...widgetSettingsPage.getDefaultSettings(),
-  //     enabled: true
-  //   };
-  //   const gutenbergBlocksSettings = {
-  //     ...defaultSettings,
-  //     priceSel: ".wc-block-components-product-price>.amount,.wc-block-components-product-price ins .amount",
-  //     locationSel: ".wc-block-components-product-price"
-  //   };
+  test('Show widget on product page', async ({ page, helper, widgetSettingsPage, dataProvider, productPage }) => {
+    // Setup
+    const { dummy_config, clear_config } = helper.webhooks;
+    await helper.executeWebhook({ webhook: clear_config }); // Clear the configuration.
+    await helper.executeWebhook({ webhook: dummy_config, args: [{ name: 'widgets', value: '0' }] }); // Setup with widgets disabled.
 
-  //   const expectWidgetsAreVisible = async (opt) => {
+    const widgetOptions = dataProvider.widgetOptions();
+    const newSettings = {
+      ...widgetOptions,
+      product: {
+        ...widgetOptions.product,
+        priceSel: '.product-info-price [data-price-type="finalPrice"] .price',
+        locationSel: '.product.info',
+        customLocations: [
+          {
+            ...widgetOptions.product.customLocations[0],
+            locationSel: '#product-addtocart-button'
+          }
+        ]
+      },
+      cart: {
+        ...widgetOptions.cart,
+        display: false, // Disable cart widgets.
+      },
+      productListing: {
+        ...widgetOptions.productListing,
+        display: false, // Disable product listing widgets.
+        useSelectors: false, // Disable selectors for product listing.
+      }
+    }
 
-  //     let i1Loc = opt.locationSel;
-  //     let i1WidgetConfig = opt.widgetConfig;
-  //     const i1CustomLoc = opt.customLocations.find(loc => loc.paymentMethod === "Paga Después")
-  //     if (i1CustomLoc) {
-  //       i1Loc = i1CustomLoc.locationSel;
-  //       i1WidgetConfig = i1CustomLoc.widgetConfig;
-  //     }
-  //     await productPage.expectWidgetToBeVisible({ ...opt, product: 'pp3' });
-  //     await productPage.expectWidgetToBeVisible({ ...opt, product: 'sp1', campaign: 'permanente' });
-  //     await productPage.expectWidgetToBeVisible({ ...opt, locationSel: i1Loc, widgetConfig: i1WidgetConfig, product: 'i1' });
-  //   }
+    const pp3Opt = {
+      locationSel: newSettings.product.locationSel,
+      widgetConfig: newSettings.widgetConfig,
+      product: 'pp3',
+      amount: 5900,
+      registrationAmount: null,
+      campaign: null
+    }
 
-  //   const customLocations = [
-  //     {
-  //       paymentMethod: "Paga Después",
-  //       display: true,
-  //       locationSel: ".single_add_to_cart_button",
-  //       widgetConfig: '{"alignment":"right","amount-font-bold":"true","amount-font-color":"#000000","amount-font-size":"15","background-color":"white","border-color":"#B1AEBA","border-radius":"5","class":"","font-color":"#1C1C1C","link-font-color":"#1C1C1C","link-underline":"true","no-costs-claim":"","size":"M","starting-text":"only","type":"banner","branding":"black"}'
-  //     }
-  //   ]
+    const sp1Opt = {
+      locationSel: newSettings.product.locationSel,
+      widgetConfig: newSettings.widgetConfig,
+      product: 'sp1',
+      amount: 5900,
+      registrationAmount: null,
+      campaign: 'permanente'
+    }
 
-  //   const themes = [
-  //     { theme: 'storefront', settings: defaultSettings }, // For classic editor with default location
-  //     { theme: 'storefront', settings: { ...defaultSettings, customLocations } }, // For classic editor with custom locations
-  //     { theme: 'twentytwentyfour', settings: gutenbergBlocksSettings }, // For gutenberg blocks with default location
-  //     { theme: 'twentytwentyfour', settings: { ...gutenbergBlocksSettings, customLocations } }, // For gutenberg blocks with custom locations
-  //   ];
+    const i1Opt = {
+      locationSel: newSettings.product.customLocations[0].locationSel || newSettings.product.locationSel,
+      widgetConfig: newSettings.product.customLocations[0].widgetConfig || newSettings.product.widgetConfig,
+      product: 'i1',
+      amount: 5900,
+      registrationAmount: null,
+      campaign: null
+    }
 
-  //   const helper = new SeQuraHelper(request, expect);
+    // Execution
+    await widgetSettingsPage.goto();
+    await widgetSettingsPage.expectLoadingShowAndHide();
+    await widgetSettingsPage.fillForm(newSettings);
+    await widgetSettingsPage.save();
 
-  //   for (const { theme, settings } of themes) {
-  //     await widgetSettingsPage.goto();
-  //     await widgetSettingsPage.expectLoadingShowAndHide();
-  //     await widgetSettingsPage.fill(settings);
-  //     // await page.pause();
-  //     await widgetSettingsPage.save({ expectLoadingShowAndHide: true, skipIfDisabled: true });
+    await productPage.goto({ slug: 'fusion-backpack' });
 
-  //     await helper.executeWebhook({ webhook: helper.webhooks.SET_THEME, args: [{ name: 'theme', value: theme }] });
-
-  //     // -- Test for simple product.
-  //     await productPage.goto({ slug: 'sunglasses' });
-  //     await expectWidgetsAreVisible({ ...settings, amount: 9000, registrationAmount: 0 });
-
-  //     // -- Test for variable product.
-  //     await productPage.goto({ slug: 'hoodie' });
-  //     await expectWidgetsAreVisible({ ...settings, amount: 8000, registrationAmount: 0 });
-
-  //     let variationOptions = [
-  //       { attributeName: 'logo', value: 'Yes', opt: { ...settings, amount: 8500, registrationAmount: 0 } }, // Variation having regular price.
-  //       { attributeName: 'logo', value: 'No', opt: { ...settings, amount: 8000, registrationAmount: 0 } } // Variation having sale price.
-  //     ];
-
-  //     for (const variationOpt of variationOptions) {
-  //       await productPage.selectVariation(variationOpt);
-  //       await expectWidgetsAreVisible(variationOpt.opt);
-  //     }
-
-  //     // -- Clear variations and test again.
-  //     await productPage.clearVariations();
-  //     await expectWidgetsAreVisible({ ...settings, amount: 8000, registrationAmount: 0 });
-  //   }
-  // });
+    await productPage.expectWidgetToBeVisible(pp3Opt);
+    await productPage.expectWidgetToBeVisible(sp1Opt);
+    await productPage.expectWidgetToBeVisible(i1Opt);
+  });
 
   // test('Don\'t show widget for banned product', async ({ productPage, generalSettingsPage, widgetSettingsPage, wpAdmin, request }) => {
   //   const helper = new SeQuraHelper(request, expect);
