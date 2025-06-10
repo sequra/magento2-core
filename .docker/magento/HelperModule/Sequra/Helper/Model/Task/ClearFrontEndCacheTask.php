1a54cb7cd58e593320a6d4e4f83e201d01bc94c9
Michel Escalante Álvarez <michel.escalante@sequra.com>
2025-06-10 12:57:26 +0200

    Add ClearFrontEndCacheTask and update webhook mapping in Webhooks class

---
+++ .docker/magento/HelperModule/Sequra/Helper/Model/Task/ClearFrontEndCacheTask.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Clear Magento's page cache
+ *
+ * @package SeQura/Helper
+ */
+
+namespace Sequra\Helper\Model\Task;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Cache\Frontend\Pool;
+use Magento\Framework\App\ResourceConnection;
+
+/**
+ * Clear Magento's page cache
+ */
+class ClearFrontEndCacheTask extends Task
+{
+
+    /**
+     * @var TypeListInterface
+     */
+    private $cacheTypeList;
+
+    /**
+     * @var Pool
+     */
+    private $cacheFrontendPool;
+
+    /**
+     * Constructor
+     *
+     * @param ResourceConnection $resourceConnection Resource connection
+     */
+    public function __construct(ResourceConnection $resourceConnection)
+    {
+        parent::__construct($resourceConnection);
+        $this->cacheTypeList = ObjectManager::getInstance()->get(TypeListInterface::class);
+        $this->cacheFrontendPool = ObjectManager::getInstance()->get(Pool::class);
+    }
+
+    /**
+     * Execute the task
+     *
+     * @param string[] $args Arguments for the task
+     *
+     * @return array<string, mixed>
+     *
+     * @throws \Exception If the task fails
+     */
+    public function execute(array $args = [])
+    {
+        $_types = [
+            'layout',
+            'block_html',
+            'collections',
+            'full_page',
+        ];
+
+        foreach ($_types as $type) {
+            $this->cacheTypeList->cleanType($type);
+        }
+        foreach ($this->cacheFrontendPool as $cacheFrontend) {
+            $cacheFrontend->getBackend()->clean();
+        }
+    }
+}
