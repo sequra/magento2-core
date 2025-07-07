e523e9e071c1aeb08283129c247c7def5b0e12e8
Boljanovic <aleksandar.boljanovic@logeecom.com>
2025-07-07 16:01:02 +0200

    Implement migration to 2.8

---
+++ Setup/Patch/Data/Version280.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Sequra\Core\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use SeQura\Core\BusinessLogic\Domain\Migration\Tasks\DeploymentMigrateTask;
+use SeQura\Core\Infrastructure\Logger\Logger;
+use Throwable;
+
+/**
+ * Class Version280.
+ *
+ * @package Sequra\Core\Setup\Patch\Data
+ */
+class Version280 implements DataPatchInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply()
+    {
+        try {
+            (new DeploymentMigrateTask())->execute();
+
+            Logger::logInfo('Migration ' . self::class . ' has been successfully finished.');
+        } catch (Throwable $e) {
+            Logger::logInfo('Update script V2.8.0.0 execution failed because: ' . $e->getMessage());
+        }
+    }
+}
